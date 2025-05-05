812e90e641c0ca9495f5ef188df3946cf8e97c79
EVERTON100792 <luismouraeverton@gmail.com>
2025-05-05 20:53:33 -0300

    Correções Exercicios

---
+++ Aula 02/EXERCICO 01/index.js
@@ -0,0 +1,1 @@
+console.log('everton','idade:', 35)
---
+++ Aula 02/EXERCICO 04/index.js
@@ -0,0 +1,6 @@
+console.log('Lista de compras:')
+console.log('- Arroz')
+console.log('- Feijão')
+console.log('- Oleo')
+
+console.log('Lista de compras: \n- Arroz \n- Feijao \n- Oleo')
