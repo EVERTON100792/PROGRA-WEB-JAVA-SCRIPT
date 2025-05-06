44a7c70203ab9247de170130d63fbe3148178532
EVERTON100792 <luismouraeverton@gmail.com>
2025-05-05 21:58:48 -0300

    f

---
+++ Aula 03- EXERCICIOS/EXERCICIO 1/index.js
@@ -0,0 +1,6 @@
+let flag = true
+console.log("Valor:", flag) imprimi o true
+console.log("Tipo:", typeof flag) imprimindo o tipo
+flag = "Hello, World!"  
+console.log("Valor:", flag) imprime o valor
+console.log("Tipo:", typeof flag) imprimi o tipo
