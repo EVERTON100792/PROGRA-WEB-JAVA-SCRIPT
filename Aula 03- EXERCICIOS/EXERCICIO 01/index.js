e9ca4f2661b6f6ed6d1ec75e546afa982655e677
EVERTON100792 <luismouraeverton@gmail.com>
2025-05-06 21:32:49 -0300

    2
    oi lindo
    tudo bem?

--- Aula 03- EXERCICIOS/EXERCICIO 01/index.js
+++ Aula 03- EXERCICIOS/EXERCICIO 01/index.js
@@ -27,6 +27,16 @@ let numero = 4;
 console.log(Numero);
 
 
+('1. Qual é a saída da função console.log() nas linhas 2, 3, 5 e 6?')
+
+let flag = true
+console.log("Valor:", flag)   ('Aqui a funcao e imprimir o nome Valor no console')
+console.log("Tipo:", typeof flag)  ('Aqui a funcão e imprimir o tipo de dado que esta sendo usado')
+flag = "Hello, World!"
+console.log("Valor:", flag)  ('aqui vai imprimir o valor ')
+console.log("Tipo:", typeof flag) ('aqui vai imprimir o tipo de dado que esta sendo usado')
+
+
 
 
 
