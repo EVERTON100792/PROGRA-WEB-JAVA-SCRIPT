c0eeb7f6be302daf7b1d620a7d075d2a95839f99
EVERTON100792 <luismouraeverton@gmail.com>
2025-05-12 16:49:51 -0300

    ..

---
+++ Aula 03/EXERCICIO 08- FEITO/index.js
@@ -0,0 +1,8 @@
+let numero = 33
+let numero_str= String
+let idade = 33
+let mensagem = "Eu tenho " + idade + " anos."
+
+console.log(mensagem)
+
+
---
+++ Aula 03/EXERCICIO 09- PENDENTE/index.js
@@ -0,0 +1,12 @@
+
+let mensagem = 'Meu nome e'
+let nome = 'Everton'
+let sobrenome = 'Moura'
+let tenho = 'Tenho'
+let idade = 32
+let nome_completo = nome + " " + sobrenome
+
+
+
+console.log(mensagem + nome_completo + tenho + idade)
+
