a7361b53a863b9ca7aa00ee3b7d85c5ee62d002b
EVERTON100792 <luismouraeverton@gmail.com>
2025-05-18 22:13:11 -0300

    Exercicios Finalizados

---
+++ Aula 04/EXERCICIO 12- FEITO/index.js
@@ -0,0 +1,28 @@
+//12. Dado um círculo com raio r = 7 , calcule sua área e circunferência. Fórmulas:
+
+//Área: A = π * r2
+//Circunferência: C = 2 * π * r
+//Considere π = 3.14 .
+
+let raio = 7
+let resultadoraio = raio * 7
+let valorpi = 3.14
+let resultadoarea = valorpi * resultadoraio
+let resultadocircunferência = 2 * valorpi * raio
+
+console.log(resultadoarea)
+console.log(resultadocircunferência)
+
+console.log(`Area do circulo: ${valorpi * resultadoraio}`)
+console.log(`Circunferencia do circulo: ${2 * valorpi * raio}`)
+
+
+
+
+
+
+
+
+
+
+
---
+++ Aula 04/EXERCICIO 13- FEITO/index.js
@@ -0,0 +1,12 @@
+//13. Um trabalhador recebe R$ 1.500,00 por mês e trabalha 8 horas por dia, 22 dias por
+//mês. Calcule o valor recebido por hora trabalhada.
+
+let salario = 1500
+let horastrabalhadas = 8
+let diastrabalhados = 22
+let horas_por_mes = horastrabalhadas * diastrabalhados
+let resultado_hora_trabalhada = salario / horas_por_mes
+
+console.log(resultado_hora_trabalhada)
+
+console.log(`Valor recebido por hora trabalhada: ${salario / horas_por_mes}`)
