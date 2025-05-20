6647c0e959ff83bb04b0e8a172f75a7d7f3bff92
EVERTON100792 <luismouraeverton@gmail.com>
2025-05-19 23:24:41 -0300

    Update index.js

--- Aula 05/EXERCICIO 03/index.js
+++ Aula 05/EXERCICIO 03/index.js
@@ -1,7 +1,7 @@
 //3. Solicite três notas e calcule a média aritmética delas.
 
-let nota01= 80 (prompt (' Digite a primeira nota'))
-let nota02= 50 (prompt (' Digite a segunda nota'))
-let nota03= 75 (prompt (' Digite a terceira nota'))
+let nota01= (prompt (' Digite a primeira nota'))
+let nota02= (prompt (' Digite a segunda nota'))
+let nota03= (prompt (' Digite a terceira nota'))
 
 console.log(`Media aritimetica= ${nota01+ nota02+ nota03 / 3}`)
