daa3712c7ad86f64f3637160d361e48a60ba1334
EVERTON100792 <luismouraeverton@gmail.com>
2025-07-06 22:04:45 -0300

    .

---
+++ EXERCICIO PARA ENTREGAR DIA 07.07/index.js
@@ -0,0 +1,37 @@
+/* Sistema de Caixa de Loja
+Implementar um sistema simples de vendas que processa múltiplas compras e gera
+relatório do dia.
+Funcionalidades:
+
+1. Menu Principal (usando switch e while)
+o Realizar venda
+o Ver relatório do dia
+o Sair
+
+2. Sistema de Vendas
+o Menu com 3 produtos com preços fixos (ex: Coca-Cola R$5.50, Chocolate
+R$3.00, etc.) e opção para finalizar a venda.
+o Calcular total da compra
+o Calcular total das vendas do dia
+o Contar o total de cada produto vendido no dia
+o Contar número total de vendas finalizadas
+
+3. Relatório do Dia
+o Total vendido
+o Número de vendas realizadas
+o Produto mais vendido
+Entregar o arquivo Javascript como anexo na atividade.*/
+
+
+
+let totaldasvendasdodia= 0
+let numerovendasfinalizadas= 0
+let contagemprodutos= 0
+let produto01= 'Coca cola'
+let produto02= 'cocolate'
+let produto03= 'Macarrao'
+let valorproduto01 = produto01 = 12.00
+let valorproduto02= produto02 = 4.50
+let valorproduto03= produto03 = 2.50
+
+ 
