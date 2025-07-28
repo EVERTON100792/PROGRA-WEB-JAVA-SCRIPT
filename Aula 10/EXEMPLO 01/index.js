4019323fe8d2ce708b17aa9f3df6f77465cba888
EVERTON100792 <luismouraeverton@gmail.com>
2025-07-28 19:59:30 -0300

    Exemplos de funcões js

---
+++ Aula 10/EXEMPLO 01/index.js
@@ -0,0 +1,22 @@
+function helooword(nome) {
+    console.log ('Hello word', nome)
+}
+
+helooword('jose');
+helooword('marcos');
+helooword('felipe');
+helooword('fabio')
+
+
+
+function soma (a,b) {
+    console.log(`Resultado: ${a+b}`)
+}
+
+soma(10,20)
+
+
+
+function soma2 (a +b) {
+    return a+b;
+}
