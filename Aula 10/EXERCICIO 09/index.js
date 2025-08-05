fb562d54325a6b3afeb21f965fab5ff0d8b149ba
EVERTON100792 <luismouraeverton@gmail.com>
2025-08-04 21:50:39 -0300

    1

--- Aula 10/EXERCICIO 09/index.js
+++
@@ -1,2 +0,0 @@
-/*9. Escreva duas funções, uma chamada ehMenor e outra ehMaior , que recebem três
-números e retorne o menor e o maior valor entre eles, respectivamente.*/
--- Aula 10/EXERCICIO 10/index.js
+++
@@ -1,5 +0,0 @@
-/*10. Escreva uma função chamada calcularDesconto que receba o valor de uma
-compra e retorne:
-10% de desconto se valor >= 100
-5% de desconto se valor >= 50
-Sem desconto se valor < 50 */
